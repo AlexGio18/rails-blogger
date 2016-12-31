3abd3707055912774cdf591c9854678e99ee4078
Alex Giacobbe <agiacobbe18@gmail.com>
2016-12-31 18:10:28 -0500

    playing with paperclip

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,10 +17,12 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-
-    flash.notice = "Article '#{@article.title}' Created!"
-    redirect_to article_path(@article)
+    if @article.save  
+      flash.notice = "Article '#{@article.title}' Created!"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -29,11 +31,13 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    @article.update(article_params)
-
-    flash.notice = "Article '#{@article.title}' Updated!"
-
-    redirect_to article_path(@article)
+    
+    if @article.update(article_params)
+      flash.notice = "Article '#{@article.title}' Updated!"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,17 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
 
-    @comment.save
+    if @comment.save
+      redirect_to article_path(@comment.article)
+    else
+      render 
+  end
 
-    redirect_to article_path(@comment.article)
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 
   private
